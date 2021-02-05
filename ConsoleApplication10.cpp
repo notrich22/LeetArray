0eab30ac1b286c813d70803c22709abbe6bfc367
notrich22 <notrich22@mail.ru>
2021-02-05 19:09:23 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApplication10.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+const int s = 10;
+int arr[s] = { 0 };
+void fill() {
+	for (int i = 0; i < s; i++) {
+		arr[i] = rand() % 100 + 1;
+	}
+}
+void showarr() {
+	for (int i = 0; i < s; i++) {
+		if(arr[i] != 0)
+			cout << arr[i] << "; ";
+	}
+}
+void delarr(int num) {
+	int t = 0;
+	for (int j = 0; j < s; j++) {
+		for (int i = 0; i < s; i++) {
+			if (arr[i] == num) {
+				while (i != s) {
+					arr[i] = arr[i + 1];
+					i++;
+				}
+			}
+		}
+	}
+}
+int main()
+{
+	srand(0);
+	fill();
+	showarr();
+	int num = 0;
+	cout << endl << "Enter num that u would like to del: ";
+	cin >> num;
+	delarr(num);
+	showarr();
+}
